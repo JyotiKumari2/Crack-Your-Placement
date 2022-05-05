f3b2b2eb3a92203a0d313eda9b13c25da601e1d8
Jyoti Kumari <jyotikm1801@gmail.com>
2022-05-05 17:47:17 +0530

    Time: 4 ms (42.58%), Space: 8.4 MB (25.63%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // Brute-Force Approach
+        // This method doesn't meets the problem constraint
+        // code starts
+        sort(nums.begin(),nums.end());
+        // code ends
+    }
+};
