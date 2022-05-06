219bae685fd5adc16dc08d336b0ab1e335c27a39
Jyoti Kumari <jyotikm1801@gmail.com>
2022-05-06 19:23:57 +0530

    Time: 57 ms (14.85%), Space: 19.3 MB (58.77%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,19 +3,34 @@ public:
     void moveZeroes(vector<int>& nums) {
         //Method 01: TC:O(n) && SC:O(n)
         // This solution doesn't meet the problem constraint
-        int count0=0;
-        vector<int>ans;
+        // int count0=0;
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]==0)
+        //           count0++;
+        //     else
+        //         ans.push_back(nums[i]);
+        // }
+        // while(count0--)
+        //     ans.push_back(0);
+        // for(int i=0;i<nums.size();i++)
+        //     nums[i]=ans[i];
+        
+        
+        // Method02: Sub-Optimal TC:O(n) && SC:O(1)
+        
+        int lastNonZeroFound=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==0)
-                  count0++;
-            else
-                ans.push_back(nums[i]);
+            if(nums[i]!=0)
+                nums[lastNonZeroFound++]=nums[i];
         }
-        while(count0--)
-            ans.push_back(0);
-        for(int i=0;i<nums.size();i++)
-            nums[i]=ans[i];
+        for(int i=lastNonZeroFound;i<nums.size();i++)
+        {
+            nums[i]=0;
+        }
+        
          
     }
 };
