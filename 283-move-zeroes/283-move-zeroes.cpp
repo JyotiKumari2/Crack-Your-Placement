849e1ce78c8a1633febe0e0eb4239ede61b652e4
Jyoti Kumari <jyotikm1801@gmail.com>
2022-05-06 19:34:44 +0530

    Time: 26 ms (72.65%), Space: 19.3 MB (14.30%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -20,15 +20,23 @@ public:
         
         // Method02: Sub-Optimal TC:O(n) && SC:O(1)
         
-        int lastNonZeroFound=0;
-        for(int i=0;i<nums.size();i++)
+        // int lastNonZeroFound=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=0)
+        //         nums[lastNonZeroFound++]=nums[i];
+        // }
+        // for(int i=lastNonZeroFound;i<nums.size();i++)
+        // {
+        //     nums[i]=0;
+        // }
+        
+        
+        // Optimal Approach : TC:O(n) && SC:O(n)
+        for(int lN0=0, i=0;i<nums.size();i++)
         {
             if(nums[i]!=0)
-                nums[lastNonZeroFound++]=nums[i];
-        }
-        for(int i=lastNonZeroFound;i<nums.size();i++)
-        {
-            nums[i]=0;
+                swap(nums[lN0++],nums[i]);
         }
         
          
