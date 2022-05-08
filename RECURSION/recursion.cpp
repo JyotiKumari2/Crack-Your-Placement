3c59454180e8e6b354b7f72779b8f63065be036d
Jyoti Kumari <jyotikm1801@gmail.com>
2022-05-08 20:24:09 +0530

    RECURSION 02-05-2022

---
+++ RECURSION/recursion.cpp
@@ -0,0 +1,16 @@
+// factorial
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n)
+{
+    if(n<=1)
+     return 1;
+     return n*factorial(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<"\n";
+    return 0;
+}
