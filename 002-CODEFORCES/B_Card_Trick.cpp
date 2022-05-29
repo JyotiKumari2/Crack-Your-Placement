6c136dc77c655b5683dfdb7e3e7a6c5fed04b4d7
Jyoti Kumari <jyotikm1801@gmail.com>
2022-05-29 12:24:41 +0530

    cf

--- 002-CODEFORCES/B_Card_Trick.cpp
+++ 002-CODEFORCES/B_Card_Trick.cpp
@@ -1,3 +1,53 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin>>t;
+//     while(t--)
+//     {
+
+
+//         // Brute force approach giving TLE
+
+//         int n;
+//         cin>>n;
+//         int *a=new int[n];
+//         for(int i=0;i<n;i++)
+//             cin>>a[i];
+//         int m;
+//         cin>>m;
+//         int *b=new int[m];
+         
+//           for(int i=0;i<m;i++)
+//           {
+//              cin>>b[i];
+             
+//           }
+
+//          for(int i=0;i<m;i++)
+//          {
+//              rotate(a,a+b[i],a+n);
+//          }
+//          cout<<a[0]<<"\n";
+            
+        
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Optimised Approach
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -6,33 +56,22 @@ int main()
     cin>>t;
     while(t--)
     {
-
-
-        // Brute force approach giving TLE
-        
-        int n;
+        int n,m,s=0;
         cin>>n;
         int *a=new int[n];
         for(int i=0;i<n;i++)
             cin>>a[i];
-        int m;
         cin>>m;
         int *b=new int[m];
-         
-          for(int i=0;i<m;i++)
-          {
+        for(int i=0;i<m;i++)
+        {
              cin>>b[i];
-             
-          }
+             s+=b[i];
+        }
+        s=s%n;
+        cout<<a[s]<<"\n";
 
-         for(int i=0;i<m;i++)
-         {
-             rotate(a,a+b[i],a+n);
-         }
-         cout<<a[0]<<"\n";
             
-        
     }
-
     return 0;
 }
