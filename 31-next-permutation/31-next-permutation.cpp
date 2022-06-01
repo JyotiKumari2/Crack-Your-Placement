f53f718fd4deb26d0ab808ca9229cd1c7d464bea
Jyoti Kumari <jyotikm1801@gmail.com>
2022-06-01 15:29:17 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (93.40%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         //Method 01: Optimised Approach TC:O(n) , SC:O(1)
-        // int n=nums.size();
-        // int ind1=-1;
-        // int ind2=-1;
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     if(nums[i-1]<nums[i])
-        //          ind1=i;
-        // }
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     if(nums[i]>nums[ind1])
-        //         ind2=ind1;
-        // }
-        // swap(nums[ind1],nums[ind2]);
-        // if(ind1<0)
-        //     reverse(nums.begin(),nums.end());
-        // else
-        // reverse(nums.begin()+ind1+1,nums.end());
+       int i=nums.size()-2;
+        while(i>=0 && nums[i+1]<=nums[i])
+              i--;
+        if(i>=0)
+        {
+            int j=nums.size()-1;
+            while(nums[j]<=nums[i])
+                 j--;
+            swap(nums[j],nums[i]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
         
         
         //Method 02 : Using In-Built Function
-        next_permutation(nums.begin(),nums.end());
+        // next_permutation(nums.begin(),nums.end());
     }
 };
