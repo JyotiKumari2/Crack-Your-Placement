7ff5ce5dc15e7a432c2ccb673481970bd7201e82
Jyoti Kumari <jyotikm1801@gmail.com>
2022-06-02 09:34:11 +0530

    Time: 8 ms (7.38%), Space: 7.2 MB (34.01%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        
+        // Brute Force Approach (Using extra space) TC:O(N*N) SC:O(N*N)
+        // int n=matrix.size();
+        // vector<vector<int>>dummyMatrix(n,vector<int>(n,0));
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         dummyMatrix[j][n-i-1]=matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         matrix[i][j]=dummyMatrix[i][j];
+        //     }
+        // }
+        
+        
+        
+        // Method 02 : Optimised Approach TC:O(N*N) SC:O(1)
+        
         int n=matrix.size();
-        vector<vector<int>>dummyMatrix(n,vector<int>(n,0));
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<i;j++)
             {
-                dummyMatrix[j][n-i-1]=matrix[i][j];
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                matrix[i][j]=dummyMatrix[i][j];
-            }
+             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
