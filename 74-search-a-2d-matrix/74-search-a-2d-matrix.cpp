03e2f469dbba11e529dc2081357e5e971e8fae59
Jyoti Kumari <jyotikm1801@gmail.com>
2022-06-03 14:06:52 +0530

    Time: 16 ms (6.49%), Space: 9.6 MB (52.64%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -16,21 +16,41 @@ public:
         // return false;
         
         
-        // METHOD 02: BINARY SEARCH || TC: O(log(m+n)) || SC: O(1)
+        // METHOD 02: BINARY SEARCH || TC: O(log(m+n)) || SC: O(1)|| MOST EFFICIENT APPROACH
         
-        int low=0;
+        // int low=0;
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // int high=m*n-1;
+        // while(low<=high)
+        // {
+        //      int mid=low+(high-low)/2;
+        //     if(matrix[mid/n][mid%n]==target)
+        //           return true;
+        //     else if(matrix[mid/n][mid%n]<target)
+        //               low=mid+1;
+        //     else
+        //         high=mid-1;
+        // }
+        // return false;
+        
+        
+        
+        //METHDO 03
+        
         int m=matrix.size();
         int n=matrix[0].size();
-        int high=m*n-1;
-        while(low<=high)
+        int i=0, j=n-1;
+        while(i<m && j>=0)
         {
-             int mid=low+(high-low)/2;
-            if(matrix[mid/n][mid%n]==target)
-                  return true;
-            else if(matrix[mid/n][mid%n]<target)
-                      low=mid+1;
+            if(matrix[i][j]==target)
+                 return true;
             else
-                high=mid-1;
+                if(matrix[i][j]>target)
+                    j--;
+            else
+                i++;
+                       
         }
         return false;
     }
