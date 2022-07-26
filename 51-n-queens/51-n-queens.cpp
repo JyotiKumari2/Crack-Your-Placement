224614e3a22903574fdb983a4e4429ede23a647a
Jyoti Kumari <jyotikm1801@gmail.com>
2022-07-26 15:46:54 +0530

    Time: 4 ms (90.36%), Space: 7.1 MB (87.97%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,40 +1,67 @@
 class Solution {
-    void solve(int col,vector<string>&board, vector<vector<string>>&ans, vector<int>&leftRow, vector<int>&upperDia, vector<int>&lowerDia, int n)
+public:
+    bool isSafe(int row, int col, vector<string>&board, int n)
     {
+        // Upper Diagonal
+        
+        
+        int dupRow=row;
+        int dupCol=col;
+        
+        while(row>=0 &&col>=0)
+        {
+            if(board[row][col]=='Q')return false;
+            row--;
+            col--;
+        }
+        
+        // Same row
+        row = dupRow;
+        col = dupCol;
+        while(col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        
+        //Lower Diagonal
+        row = dupRow;
+        col = dupCol;
+        while(row<n && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+        
+        return true;
+    }
+    
+    void solve( int col, int n , vector<vector<string>>&ans, vector<string>&board)
+    {
+        // base case
         if(col==n)
         {
             ans.push_back(board);
-            return ;
+            return;
         }
-        
         for(int row=0;row<n;row++)
         {
-            if(leftRow[row]==0 && lowerDia[row+col]==0 && upperDia[n-1+col-row]==0)
+            if(isSafe(row,col,board,n))
             {
                 board[row][col]='Q';
-                leftRow[row]=1;
-                lowerDia[row+col]=1;
-                upperDia[n-1+col-row]=1;
-                solve(col+1,board,ans,leftRow,upperDia,lowerDia,n);
+                solve(col+1,n,ans,board);
                 board[row][col]='.';
-                leftRow[row]=0;
-                lowerDia[row+col]=0;
-                upperDia[n-1+col-row]=0;
             }
         }
     }
-public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         vector<string>board(n);
         string s(n,'.');
         for(int i=0;i<n;i++)
-        {
-            board[i]=s;
-        }
-        
-        vector<int>leftRow(n,0), upperDia(2*n-1,0), lowerDia(2*n-1,0);
-        solve(0,board,ans,leftRow,upperDia,lowerDia,n);
+                board[i]=s;
+        solve(0,n,ans,board);
         return ans;
     }
 };
