a987d1c7cd19e1b728b5d271f6aff86c1b1ade43
Jyoti Kumari <jyotikm1801@gmail.com>
2022-08-24 20:22:33 +0530

    day wasted

--- 002-CODEFORCES/A_Next_Round.cpp
+++ 002-CODEFORCES/A_Next_Round.cpp
@@ -1,29 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n,k;
     cin>>n>>k;
     int count=0;
-     int *a=new int[n];
-    int s=0;
+     int *arr=new int[n];
+    // int s=0;
     for(int i=0;i<n;i++)
       {
-        cin>>a[i];
-        s+=a[i];
+        cin>>arr[i];
+        // s+=a[i];
       }
 
-      if(s==0)
-         cout<<0<<"\n";
-         else{
-            for(int i=0;i<n;i++)
-            {
-                if(a[i]>=a[k-1])
-                    count++;
-            }
-               cout<<count<<"\n";
-         }
-        
-        
-    return 0;
+
+
+ 
+   
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]>=arr[k-1] && arr[i]!=0)count++;
+    }
+    cout<<count;
+return 0;
 }
+ 
