24fa36d01b33004591ec431d23a324f0e133c551
Jyoti Kumari <jyotikm1801@gmail.com>
2022-09-16 22:42:17 +0530

    day70 :( Its a complete waste day

--- Practice/B_Make_It_Increasing.cpp
+++ Practice/B_Make_It_Increasing.cpp
@@ -181,7 +181,22 @@ void printSubsequence(int i , vector<int>&ds, vector<int>arr, int n)
 //Code:
 void solve()
 {
-  
+  int n;
+    cin >> n;
+    vector<int>a(n);
+    for(auto &i : a) cin >> i;
+    int ans = 0;
+    for(int i = n - 2; i >= 0; i--){
+        while(a[i] >= a[i + 1] && a[i] > 0){
+            a[i] /= 2;
+            ans++;
+        }
+        if(a[i] == a[i+1]){
+            cout << -1 << '\n';
+            return;
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main()
