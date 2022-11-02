c3b684b57f1bc167cd730d33b91c772e77ec832f
Jyoti Kumari <jyotikm1801@gmail.com>
2022-11-02 20:09:58 +0530

    Time: 0 ms (100.00%), Space: 8 MB (99.52%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //Method 01 : Optimised Method : Using 3 pointer approach
+        
+        int n=nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        
+        while(mid<=high)
+        {
+             switch(nums[mid])
+             {
+                 case 0: swap(nums[mid++],nums[low++]);
+                          break;
+                 case 1:  mid++;
+                          break;
+                 case 2: swap(nums[mid],nums[high--]);
+                          break;
+             }
+        }
+    }
+};
