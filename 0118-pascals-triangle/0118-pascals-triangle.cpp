f6ee0b41e4dec2f9e029c9de7e6474c4e073c184
Jyoti Kumari <jyotikm1801@gmail.com>
2022-11-27 19:30:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (70.47%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>pascaltriangl(numRows);
+        vector<vector<int>>pT(numRows);
         for(int i=0;i<numRows;i++)
         {
-            pascaltriangl[i].resize(i+1);
-            pascaltriangl[i][0]=pascaltriangl[i][i]=1;
+            pT[i].resize(i+1);
+            pT[i][0]=pT[i][i]=1;
             for(int j=1;j<i;j++)
             {
-                pascaltriangl[i][j]=pascaltriangl[i-1][j-1]+pascaltriangl[i-1][j];
+                pT[i][j]=pT[i-1][j-1]+pT[i-1][j];
             }
         }
-        return pascaltriangl;
+        return pT;
     }
 };
