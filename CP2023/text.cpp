322889b792b060b17e7e2ec845c6b8ffd427f1d9
Jyoti Kumari <jyotikm1801@gmail.com>
2023-01-25 10:39:23 +0530

    25Jan

---
+++ CP2023/text.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	#define ONLINE_JUDGE
+	//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	//for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+
+	int n;
+	cin>>n;
+	cout<<n;
+	return 0;
+}
